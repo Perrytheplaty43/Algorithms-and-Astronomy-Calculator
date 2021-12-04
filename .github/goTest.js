3d6f4bb0244cc2c2e3d1d587338ac94e254d9393
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-12-04 15:18:06 -0800

    fixing test for rounding errors

--- .github/goTest.js
+++ .github/goTest.js
@@ -10,13 +10,14 @@ globalThis.child = child
 let finished = false
 
 const expectedOutput = "[[2281 76 5 Open star cluster Auriga] [2403 72 8 Galaxy Camelopardalis] [2683 73 9 Galaxy Lynx] [2841 76 9 Galaxy Ursa Major]]"
+const expectedOutput2 = "[[2281 76 5 Open star cluster Auriga] [2403 72 8 Galaxy Camelopardalis] [2841 76 9 Galaxy Ursa Major] [2683 73 9 Galaxy Lynx]]"
 
 console.log("Starting Go server...")
 child.exec('go run /home/runner/work/Algorithms-and-Astronomy-Calculator/Algorithms-and-Astronomy-Calculator/Go/server.go test', (err, stdout, stderr) => {
     if (err) {
         throw err
     }
-    if (stdout == expectedOutput) {
+    if (stdout == expectedOutput || stdout == expectedOutput2) {
         console.log("Success")
     } else {
         throw stdout
