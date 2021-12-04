cba048ade4895b22b46c7ba309d3c0662babc421
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-12-03 23:26:35 -0800

    fixign

--- Go/server.go
+++ Go/server.go
@@ -42,7 +42,10 @@ func astroHandler(w http.ResponseWriter, r *http.Request) {
 		tolMag, _ := strconv.ParseFloat(r.Form["tolMag"][0], 64)
 		date := r.Form["date"][0]
 		types := strings.Split(r.Form["type"][0], ",")
-		dirname := os.Args[1]
+		var dirname string
+		if len(os.Args) > 0 {
+			dirname = os.Args[1]
+		}
 		var records [][]string
 		fmt.Println(dirname)
 		if dirname == "test" {
