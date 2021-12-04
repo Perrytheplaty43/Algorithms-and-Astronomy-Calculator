ed8d9845238715849077905d8da885450848b1a7
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-12-03 22:37:05 -0800

    logging

--- app.js
+++ app.js
@@ -371,8 +371,9 @@ const server = home.startsWith('/home/runner/') ?
         curlTest("/MineSweeper/MineSweeperWWW/css/index.css")
         child.exec('curl -X GET "http://127.0.0.1:8000/astro?lat=47.740372&long=-122.222695&tol=70&tolMag=10&type=Gx,OC,Gb,Nb,Pl,CpN,Ast,Kt,TS,DS,SS,Q,U,D,PD&date=2100-10-16"', (err, stdout, stderr) => {
             finished++;
+            console.log("Geting algorithms__")
             if (!stdout.startsWith("<!-- 404 -->") && err == null) {
-                console.log("Geting \'" + path + "\': Success")
+                console.log("Geting algorithms: Success")
             } else {
                 throw stderr;
             }
