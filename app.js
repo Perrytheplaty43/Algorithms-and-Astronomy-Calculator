d449b9d1f5c64ae2c76ce556652626d7e8795e49
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-12-11 17:00:00 -0800

    formating

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ const home = process.cwd()
 const frameworkPath1 = "/MineSweeper/MineSweeperWWW/_framework";
 const frameworkPath2 = "/_framework";
 let fileCount;
+
 if (!home.startsWith('/home/runner')) {
     fs.readFile(home + delimiter + 'Logs' + delimiter + 'fileCount.txt', function (err, html) {
         fileCount = parseInt(html)
