01f48ad091530b00f88593435873f50a514bee8f
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-12-16 20:49:42 -0800

    fixing colors?

--- astroTargetFinder/script.js
+++ astroTargetFinder/script.js
@@ -526,12 +526,15 @@ function save(inputs, timesUNIX) {
     if (clouds[clouds.length - 1] < 10) {
         condition = "Perfect"
         document.getElementById("condition").classList.add('p1green')
+        document.getElementById("condition").classList.remove('p1')
     } else if (((() => { let turning = 0; for (i = 0; i <= clouds.length - 1; i++) { turning += clouds[i]; } return turning })()) / clouds.length < 30) {
         condition = "Fair"
         document.getElementById("condition").classList.add('p1yellow')
+        document.getElementById("condition").classList.remove('p1')
     } else if (clouds.length == 0) {
         condition = "Unknown (too far in the future)"
         document.getElementById("condition").classList.add('p1red')
+        document.getElementById("condition").classList.remove('p1')
     } else condition = "Bad"
     document.getElementById("condition").innerHTML = "Weather Contition: " + condition;
 }
