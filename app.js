aac107869b50800c4cc8fa5b3304d6e5ad05424b
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-12-22 01:18:02 -0800

    testing

--- app.js
+++ app.js
@@ -349,11 +349,9 @@ function myServer(req, res) {
                 let data = JSON.parse(finalData);
                 let writing = []
                 for (i = 0; i <= data.length - 1; i++) {
-                    writing.push({ [i]: { id: data[i][0], alt: data[i][1], mag: data[i][2], type: data[i][3], constellation: data[i][4] } })
+                    writing.push({ [i]: { id: data[i][0], alt: data[i][1], mag: data[i][2], type: data[i][3], constellation: data[i][4], toString: function(){return [this.id,  this.alt, this.mag, this.ityped, this.constellation]} } })
                 }
-                let toWriting = JSON.stringify(writing).join("")
-                toWriting = toWriting.replace("}},", "}} ")
-                res.write(toWriting);
+                res.write(JSON.stringify(writing.join("")));
                 res.end();
             })
             .catch(error => console.log('error:', error));
