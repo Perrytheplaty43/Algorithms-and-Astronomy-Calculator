c297b39454c898a9acf720cf2b1304fcacb8b049
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-12-23 00:45:30 -0800

    testing

--- app.js
+++ app.js
@@ -342,8 +342,7 @@ function myServer(req, res) {
         let date = searchParams.get('date')
         res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
         let isWeatherGoodReturn = isWeatherGood(lat, long, date)
-        console.log(await isWeatherGoodReturn)
-        res.write(JSON.stringify({ conditions: isWeatherGoodReturn }));
+        const writer = isWeatherGoodReturn.then(res.write(JSON.stringify({ conditions: isWeatherGoodReturn })));
         res.end();
         return;
     }
