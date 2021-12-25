8bff29a8c46620224a0f2834bbbc8fb0f719f016
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-12-24 23:48:20 -0800

    never mind

--- app.js
+++ app.js
@@ -427,9 +427,7 @@ function myServer(req, res) {
             })
             .then(r => {
                 let saved = save(r, timesUNIX)
-                console.log(astro, "  sfsfsfsdfsfsfdfsd")
                 if (!astro) {
-                    
                     res.write(JSON.stringify({ conditions: saved }))
                     res.end();
                 }
