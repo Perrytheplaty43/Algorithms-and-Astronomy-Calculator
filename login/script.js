babb13a5b06ae280a5a3ab0a8692f69d92505e9b
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-12-26 02:26:40 -0800

    testing

--- login/script.js
+++ login/script.js
@@ -1,10 +1,12 @@
 function submit(event) {
-    fetch(
-        'https://' + window.location.hostname + '/api/login?user=' + document.getElementById("user").value + '&pass=' + document.getElementById("pass").value,
-        { method: 'POST' }
-    )
-        .then(alert("suc"))
-        .catch(error => console.log('error:', error));
-    event.preventDefault();
+    if (event.submitter.id == "submit") {
+        fetch(
+            'https://' + window.location.hostname + '/api/login?user=' + document.getElementById("user").value + '&pass=' + document.getElementById("pass").value,
+            { method: 'POST' }
+        )
+            .then(alert("suc"))
+            .catch(error => console.log('error:', error));
+        event.preventDefault();
+    }
     return false
 }
