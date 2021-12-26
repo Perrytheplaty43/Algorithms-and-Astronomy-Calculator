7826ca8e6a650e60a44ae3bbafd87bc7d16aeff6
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-12-26 01:47:26 -0800

    testing

--- app.js
+++ app.js
@@ -62,6 +62,11 @@ await docRef.set({
     born: 1815
 });
 
+const snapshot = await db.collection('users').get();
+snapshot.forEach((doc) => {
+  console.log(doc.id, '=>', doc.data());
+});
+
 function myServer(req, res) {
     const { method, url } = req;
     if (!testing) {
