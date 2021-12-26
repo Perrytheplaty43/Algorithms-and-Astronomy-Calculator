38f24c7f6fc430fe7627103d87311754d4f017ee
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-12-26 02:36:53 -0800

    fixing

--- login/script.js
+++ login/script.js
@@ -1,12 +1,12 @@
-function submit(event) {
+function onSubmit(event) {
     if (event.submitter.id == "submit") {
+        event.preventDefault();
         fetch(
             'https://' + window.location.hostname + '/api/login?user=' + document.getElementById("user").value + '&pass=' + document.getElementById("pass").value,
             { method: 'POST' }
         )
             .then(alert("suc"))
             .catch(error => console.log('error:', error));
-        event.preventDefault();
     }
     return false
 }
