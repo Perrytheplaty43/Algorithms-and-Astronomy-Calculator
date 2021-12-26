e9dc40dde3d8daa05fe677b203f748caa1201174
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-12-26 12:37:30 -0800

    test

--- app.js
+++ app.js
@@ -659,7 +659,7 @@ function myServer(req, res) {
         const docRef = db.collection('users').doc(user);
         let doc = await docRef.get()
 
-        console.log(doc, "___________________________")
+        console.log(doc.QueryDocumentSnapshot, "___________________________")
         return await docRef.update({
             fav: id
         })
