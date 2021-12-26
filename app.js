582cdc18dfd35b7b8a43f38a9c44b169f4dff327
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-12-26 10:39:49 -0800

    renaming

--- app.js
+++ app.js
@@ -156,7 +156,7 @@ function myServer(req, res) {
         return;
     }
     if (method == 'GET' && surl.pathname == '/signup/style.css') {
-        fs.readFile(home + delimiter + 'login' + delimiter + 'style.css', function (err, html) {
+        fs.readFile(home + delimiter + 'signup' + delimiter + 'style.css', function (err, html) {
             if (err) {
                 console.log(err);
                 errorLog(testing, err, "2")
@@ -169,7 +169,7 @@ function myServer(req, res) {
         return;
     }
     if (method == 'GET' && surl.pathname == '/signup/script.js') {
-        fs.readFile(home + delimiter + 'login' + delimiter + 'script.js', function (err, html) {
+        fs.readFile(home + delimiter + 'signup' + delimiter + 'script.js', function (err, html) {
             if (err) {
                 console.log(err);
                 errorLog(testing, err, "2")
@@ -182,7 +182,7 @@ function myServer(req, res) {
         return;
     }
     if (method == 'GET' && surl.pathname == '/signup/') {
-        fs.readFile(home + delimiter + 'login' + delimiter + 'index.html', function (err, html) {
+        fs.readFile(home + delimiter + 'signup' + delimiter + 'index.html', function (err, html) {
             if (err) {
                 console.log(err);
                 errorLog(testing, err, "2")
