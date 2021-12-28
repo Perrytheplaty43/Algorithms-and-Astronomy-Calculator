fbbe22c1999fc12d52050412a83aa45278bf1b9c
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-12-28 15:29:35 -0800

    try again

--- app.js
+++ app.js
@@ -1101,7 +1101,7 @@ function myServer(req, res) {
                     tokenEx: currentDate.toString()
                 })
                 var mailOptions = {
-                    from: '"Astronomy Calculator " <astronomycalculatorreset@zohomail.com>',
+                    from: '"astronomycalculatornoreply" <astronomycalculatorreset@zohomail.com>',
                     to: doc.data().email,
                     subject: 'Reset Password ',
                     html: 'Click <a href="https://' + addr + '/forgot?token=' + doc.data().token + '">here</a> to reset password'
