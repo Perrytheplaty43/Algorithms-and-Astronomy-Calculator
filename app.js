1edd6253fa5185b49ee03a7eff13fc2979a8b7ed
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-12-31 16:57:51 -0800

    fixing bug

--- app.js
+++ app.js
@@ -1093,10 +1093,10 @@ function myServer(req, res) {
             let month;
             let day;
             let now = new Date()
-            if  (now.getMonth() + 1 >= 1 || now.getMonth() + 1 <= 9) {
-                month = "0" + now.getMonth() + 1
+            if  ((now.getMonth() + 1) >= 1 || (now.getMonth() + 1) <= 9) {
+                month = "0" + (now.getMonth() + 1)
             } else {
-                month = now.getMonth() + 1
+                month = (now.getMonth() + 1)
             }
             if  (now.getDate() >= 1 || now.getDate() <= 9) {
                 day = "0" + now.getDate()
