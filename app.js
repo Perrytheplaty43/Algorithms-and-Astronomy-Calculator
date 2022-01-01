69b3dd8afcb530530adf9d609e1a68f28783b5c2
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-12-31 16:47:15 -0800

    debugging

--- app.js
+++ app.js
@@ -1009,6 +1009,7 @@ function myServer(req, res) {
             .then(response => response.text())
             .then(data => {
                 res.writeHead(200, { 'Content-Type': 'text/json' });
+                console.log(data)
                 res.write(JSON.stringify({ moonrise: JSON.parse(data).location.time[0].moonrise.time, moonset: JSON.parse(data).location.time[0].moonset.time, phase: JSON.parse(data).location.time[0].moonphase.value }));
                 res.end();
             })
