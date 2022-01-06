9467ec4caf0eddd8aff35e9c1f3dfad9c047430c
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2022-01-05 19:27:52 -0800

    adding better logs

--- app.js
+++ app.js
@@ -525,6 +525,14 @@ function myServer(req, res) {
             if (err) {
                 console.log(err);
                 errorLog(testing, err, "11")
+                fs.appendFile(home + delimiter + 'Logs' + delimiter + 'todo.txt', "\n" + id, (err) => {
+                    if (err) {
+                        console.log(err);
+                        errorLog(testing, err, "20")
+                        return;
+                    }
+                    return;
+                })
                 return;
             }
             let date = new Date();
