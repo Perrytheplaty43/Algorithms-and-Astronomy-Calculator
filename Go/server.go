9606fa3847c5a8216c19bab4c7777904b8277ec1
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2022-04-30 12:26:16 -0700

    testing

--- Go/server.go
+++ Go/server.go
@@ -338,8 +338,9 @@ func formOutput(avgArray [][]interface{}, data [][]string, minAccALT float64, to
 		if final[i][4] == nil {
 			final[i][4] = "Not in constellation!!"
 		}
-		magFloat,_ := strconv.ParseFloat(final[i][2].(string), 64)
-		final[i] = append(final[i], (((((final[i][1].(float64) - minAccALT) / 90) * 75) + ((tolMag - (magFloat + 30))) / 95) * 25))
+		magFloat, _ := strconv.ParseFloat(final[i][2].(string), 64)
+		//final[i] = append(final[i], (((((final[i][1].(float64) - minAccALT) / 90) * 75) + (tolMag-(magFloat+30))/95) * 25))
+		final[i] = append(final[i], 75*(final[i][1].(float64)/90)+(100-75)*(1-((magFloat+10)/40)))
 	}
 	fmt.Printf("%v", final)
 	return final
